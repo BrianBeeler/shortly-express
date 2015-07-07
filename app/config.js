bce4c93c4ef5ac17108a5c966f55d5196603a771
BrianBeeler <brianlbeeler@gmail.com>
2015-07-06 19:28:21 -0500

    User Table Created

--- app/config.js
+++ app/config.js
@@ -41,6 +41,20 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 100);
+      user.string('password', 100);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created User Table', table);
+    });
+  }
+});
+
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,22 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      var username = model.get('username')
+      var password = model.get('password')
+
+      bcrypt.hash(username, null, null, function(err, results){
+        model.set('username', results)
+      })
+      bcrypt.hash(password, null, null, function(err, results){
+        model.set('password', results)
+      })
+    })
+  }
 });
 
 module.exports = User;
